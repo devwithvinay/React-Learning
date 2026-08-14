18107048e2820dda20978d46e0a67b3b9d75b7f1
vinay-here <krvinay785@gmail.com>
2026-08-14 10:27:10 +0530

    provider , useContext

--- weeks/week2/src/contextApi/ContextApi.jsx
+++ weeks/week2/src/contextApi/ContextApi.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
+import React,{createContext, useContext, useState} from 'react'
+
+const BulbContext =  createContext();
 
 function ContextAPI() {
+  const [bulbOn , setBulbOn] = useState(true)
   return (
     <div>
-      HIii There this is about context api
+      <BulbContext.Provider
+        value={{
+          bulbOn: bulbOn,
+          setBulbOn: setBulbOn,
+        }}
+      >
+        <Light />
+      </BulbContext.Provider>
+    </div>
+  );
+}
+
+function Light(){
+  return (
+    <div>     
+        <LightBulb />
+        <LightSwitch />
+    </div>
+  );
+}
+
+function LightBulb(){
+  const { bulbOn } = useContext(BulbContext)
+  return (
+    <div>
+      { bulbOn ? "Bulb On" : "Bulb Off" }
     </div>
   )
 }
+
+function LightSwitch(){
+  const {bulbOn , setBulbOn} = useContext(BulbContext)
+  return(
+    <div>
+      <button onClick={()=>setBulbOn(!bulbOn)}> Switch Bulb</button>
+    </div>
+  )
+}
+
  
 export default ContextAPI
 
