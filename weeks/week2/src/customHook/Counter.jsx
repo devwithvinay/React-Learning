84dd0fe05e98a5fed593f552260af6a26499d3d6
vinay-here <krvinay785@gmail.com>
2026-08-15 17:52:58 +0530

    counter Custom hook

--- weeks/week2/src/customHook/Counter.jsx
+++ weeks/week2/src/customHook/Counter.jsx
@@ -1,38 +1,37 @@
-import React,{useState} from 'react'
-
-// Custom Hook 
-function useCounter(){
-    const [count, setCount] = useState(0);
-
-     function increaseCount() {
-       setCount((c) => c + 1);
-     }
-
-     return {
-        count : count ,
-        increaseCount : increaseCount
-     }
-}
+import React, {useState} from "react"
 
 function Counter() {
   return (
     <div>
       <Counting />
-      <Counting />
-      <Counting />
+      <br/>
       <Counting />
     </div>
   );
+}
+
+function useCounter (){
+  const [ count , setCount ] = useState(0);
+
+  function increaseCount(){
+    setCount(c=>c+1)
+
+  }
+  return {
+    count :count,
+    increaseCount : increaseCount
+  }
+
 }
 
 function Counting(){
-  const { count, increaseCount } = useCounter(0);
-  return (
-    <div>
-      <button onClick={increaseCount}>Increase {count} </button>
-    </div>
-  );
 
+  const {count , increaseCount} = useCounter()
+  return <div>
+    <button onClick={increaseCount}>Count:{count}</button>
+  </div>
 }
+
+
 
 export default Counter
