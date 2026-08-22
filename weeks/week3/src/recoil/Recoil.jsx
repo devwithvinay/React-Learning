1cc3d7295919f688e0ccd1fb8ba8e52ce1cbbb02
vinay-here <krvinay785@gmail.com>
2026-08-22 23:17:11 +0530

    Recoil ,Atom

--- weeks/week3/src/recoil/Recoil.jsx
+++ weeks/week3/src/recoil/Recoil.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { counterAtom } from '../store/atoms/counter';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 function Recoil() {
   return (
     <div>
-      
+      <Counter/>
     </div>
   )
 }
+
+
+function Counter(){ 
+   return (
+    <div>
+      <CurrentCounter  />
+      <Increase  />
+      <Decrease  />
+    </div>
+  );
+}
+
+function CurrentCounter() {
+
+ const count = useRecoilValue(counterAtom)
+ 
+  return <div>{count}</div>;
+}
+
+function Increase() {
+
+  const setCount = useSetRecoilState(counterAtom)
+
+  return (
+    <div>
+      <button onClick={() => setCount((c) => c + 1)}>Increase</button>
+    </div>
+  );
+}
+
+function Decrease() {
+
+  const setCount = useSetRecoilState(counterAtom)
+
+  return (
+    <div>
+      <button onClick={() => setCount((c) => c - 1)}>Decrease</button>
+    </div>
+  );
+}
+
+
 
 export default Recoil 
